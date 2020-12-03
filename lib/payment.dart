2acf4b58522b84974e23a2883a384159fc3c850c
SdaemonDev <daminijadhav1213@gmail.com>
2020-12-03 19:25:26 +0530

    payment.dart

--- lib/payment.dart
+++ lib/payment.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:square_in_app_payments/models.dart';
 import 'package:square_in_app_payments/in_app_payments.dart';
@@ -9,6 +11,8 @@ class SquarePaymentExample extends StatefulWidget {
 
 class _SquarePaymentExampleState extends State<SquarePaymentExample> {
 
+  bool _googlePayEnambled=false;
+
   void _pay(){
       InAppPayments.setSquareApplicationId("sandbox-sq0idb-9faP3OqiTVI-qNbUYce14Q");
       InAppPayments.startCardEntryFlow(
@@ -17,6 +21,46 @@ class _SquarePaymentExampleState extends State<SquarePaymentExample> {
       );
   }
 
+  Future<void> _initsquareGooglePay() async{
+    var canUseGooglePay = false;
+    if(Platform.isAndroid) {
+      await InAppPayments.initializeGooglePay('LOCATION_ID',1);
+      canUseGooglePay=await InAppPayments.canUseGooglePay;
+    }
+    setState(() {
+      _googlePayEnambled=canUseGooglePay;
+    });
+  }
+
+  void _onStartGooglePay() async {
+    try {
+      InAppPayments.setSquareApplicationId("sandbox-sq0idb-9faP3OqiTVI-qNbUYce14Q");
+      await InAppPayments.requestGooglePayNonce(
+            price: '1.0',
+            currencyCode: 'USD',
+            onGooglePayNonceRequestSuccess:_onGooglePayNonceRequestSuccess ,
+            onGooglePayNonceRequestFailure:_onGooglePayNonceRequestFailure ,
+            priceStatus: 1);
+    } on InAppPaymentsException catch(ex){
+          print("Exception ${ex.toString()}");
+    }
+  }
+
+  void _onGooglePayNonceRequestSuccess(CardDetails result) async {
+    try {
+      // take payment with the card nonce details
+      // you can take a charge
+      // await chargeCard(result);
+
+    } on Exception catch (ex) {
+      // handle card nonce processing failure
+    }
+  }
+
+
+  void _onGooglePayNonceRequestFailure(ErrorInfo errorInfo) {
+    // handle google pay failure
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -27,6 +71,16 @@ class _SquarePaymentExampleState extends State<SquarePaymentExample> {
       floatingActionButton: FloatingActionButton(
         onPressed: _pay,
         child: Icon(Icons.payment),
+      ),
+      body: Column(
+        children: [
+          FlatButton(
+            child: Text('google pay',style: TextStyle(color: Colors.red),),
+            onPressed: (){
+              _onStartGooglePay();
+            },
+          )
+        ],
       ),
     );
   }
